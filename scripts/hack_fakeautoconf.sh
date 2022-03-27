d7d0511fd1064f97308b41ee0577fb4f57861db4
brain-zhang <brain.zhangbin@outlook.com>
2022-03-27 10:15:36 +0800

    for windows compile

--- scripts/hack_fakeautoconf.sh
+++ scripts/hack_fakeautoconf.sh
@@ -3,8 +3,11 @@
 # Specifically for windows, where installing autoconf looks suspiciously
 # like boiling the ocean.
 
+N2N_VERSION_SHORT='happynet3.0.0'
+GIT_COMMITS=`git rev-list --count HEAD`
+
 sed \
-    -e "s%@N2N_VERSION_SHORT@%$FIXME%g" \
+    -e "s%@N2N_VERSION_SHORT@%N2N_VERSION_SHORT%g" \
     -e "s%@GIT_COMMITS@%$GIT_COMMITS%g" \
     -e "s%@CC@%gcc%g" \
     -e "s%@AR@%ar%g" \
@@ -18,7 +21,7 @@ sed \
     < tools/Makefile.in > tools/Makefile
 
 cat <<EOF >include/config.h
-#define PACKAGE_VERSION "$FIXME"
-#define PACKAGE_OSNAME "$FIXME"
-#define GIT_RELEASE "$FIXME"
+#define PACKAGE_VERSION $N2N_VERSION_SHORT
+#define PACKAGE_OSNAME "Windows"
+#define GIT_RELEASE $N2N_VERSION_SHORT
 EOF
